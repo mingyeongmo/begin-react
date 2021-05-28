a006e289e9949d009cbb4323e26717dbe9c5e33f
mingyeongmo <mingyeongmp@naver.com>
2021-05-28 11:11:07 +0900

    useMemo를 쓰기전엔 불필요한 리렌더링이 된다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, {useRef, useState} from 'react';
 import UserList from './UserList'
 import CreateUser from './CreateUser';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -16,7 +21,7 @@ function App() {
       ...inputs,
       [name]: value
     });
-  }
+  };
 
   const [users, setUsers] = useState([
     {
@@ -70,6 +75,8 @@ function App() {
     );
   };
 
+  const count = countActiveUsers(users);
+
   return (
     <>
       <CreateUser 
@@ -79,6 +86,7 @@ function App() {
         onCreate={onCreate}
         />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
